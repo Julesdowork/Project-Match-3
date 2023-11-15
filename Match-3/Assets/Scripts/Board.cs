533358c0693283e71b0bbe40bcbad3447edcc2aa
Julian McNeill <julesdowork@gmail.com>
2023-11-15 18:39:31 -0500

    Getting the Board

---
+++ Match-3/Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+
+    private BackgroundTile[,] _allTiles;
+
+    void Start()
+    {
+        _allTiles = new BackgroundTile[width, height];
+        Setup();
+    }
+
+    private void Setup()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            }
+        }
+    }
+}
